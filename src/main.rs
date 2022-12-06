4abd9574a6f1efe9e6210a96a0d803757d45fc12
ptanlam <phanlam20033002@gmail.com>
2022-12-06 21:30:15 +0700

    add background

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::{error::Error, io, time::Duration};
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
 
 use crossterm::{
     cursor::{Hide, Show},
     event::{self, Event, KeyCode},
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
+};
+use invaders::{
+    frame::{self, new_frame},
+    render,
 };
 use rusty_audio::Audio;
 
@@ -23,7 +27,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    // ! In real project, should use channel from crossbeam lib
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
+        // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -35,8 +63,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw & Render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
